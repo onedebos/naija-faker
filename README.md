123b1bd591e58c2113c7d3816a54ce245b1b42de
Adebola <onedebos@gmail.com>
2020-07-15 14:38:07 +0100

    implement all functions

---
+++ README.md
@@ -0,0 +1,81 @@
+# naija-faker
+
+This package helps you generate random Nigerian names on the fly for use in seeding your DB or for loading data into your apps.
+
+# Getting Started
+
+**To get started, follow the instructions below**
+
+- install the package `npm install naija-faker`
+- import it in your app using either of:
+
+```
+ import naijaFaker from 'naija-faker // if using ES6
+
+ const naijaFaker = require('naija-faker)
+```
+
+- use it! `naijaFaker.getFirstName()`
+
+# Available methods
+
+## Generate a random First name or Last name
+
+- `getFirstName() //=> 'abraham'`
+- `getLastName() //=> 'fatai`
+
+## Generate a random First name or Last name that starts with a specific letter
+
+NB: Currently only accepts one letter
+
+- `getFirstName('r') //=> 'rahman'`
+- `getLastName('b') //=> 'bolanle'`
+
+## Generate a person Object with random values for First name, Last name and Age
+
+NB: Default age is between 18 - 50
+
+- `getPerson() //=> {fName: 'abdul', lName: 'qadr', age: 20 }`
+
+-- To change the default `min` and `max` values for age, pass in min and max values as an object like below.
+
+- e.g: `getPerson({min:20, max:50}) //=> {fName: 'abdul', lName: 'qadr', age: 40 }`
+
+## Generate an array of people with first name, last name and age
+
+- To generate an array of people, simply pass an `amt` value to `getPerson()` i.e `getPersonList({amt: 2})`
+
+```
+getPersonList({amt: 2}) //=> [{fName: 'abdul', lName: 'qadr', age: 40 }, {fName: 'frank', lName: 'edoho', age: 23 }]`
+```
+
+## Generate an array of only names
+
+- Simply pass `amt` to `getNameList()` e.g
+
+```
+getNameList({amt: 5})
+```
+
+## Authors
+
+👤 **Adebola Adeniran**
+
+- Github: [@githubhandle](https://github.com/onedebos)
+- Twitter: [@twitterhandle](https://twitter.com/debosthefirst)
+- Linkedin: [linkedin](https://www.linkedin.com/in/adebola-niran/)
+- Web: [Adebola](https://adebola.dev)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
---
+++ index.js
@@ -0,0 +1,221 @@
+const path = require("path");
+const { firstNames, lastNames } = require(path.join(__dirname, "names.js"));
+
+const getFirstName = (letterToStartWith) => {
+  let max = firstNames.length - 1;
+  let min = 0;
+  let position;
+  // random fName
+  if (!letterToStartWith) {
+    position = Math.floor(Math.random() * (max - min) + min);
+    return firstNames[position];
+  } else {
+    // random fName that starts with letterToStartWith
+    if (letterToStartWith.length <= 1) {
+      const arrayOfNamesThatStartsWithLetter = [];
+      firstNames.map((firstName) => {
+        if (firstName.startsWith(letterToStartWith)) {
+          arrayOfNamesThatStartsWithLetter.push(firstName);
+        }
+      });
+
+      max = arrayOfNamesThatStartsWithLetter.length;
+      min = 0;
+      position = Math.floor(Math.random() * (max - min) + min);
+      return arrayOfNamesThatStartsWithLetter[position];
+    }
+  }
+};
+
+const getLastName = (letterToStartWith) => {
+  let max = lastNames.length - 1;
+  let min = 0;
+  let position;
+  // random fName
+  if (!letterToStartWith) {
+    position = Math.floor(Math.random() * (max - min) + min);
+    return lastNames[position];
+  } else {
+    // random fName that starts with letterToStartWith
+    if (letterToStartWith.length <= 1) {
+      const arrayOfNamesThatStartsWithLetter = [];
+      lastNames.map((lastName) => {
+        if (lastName.startsWith(letterToStartWith)) {
+          arrayOfNamesThatStartsWithLetter.push(lastName);
+        }
+      });
+
+      max = arrayOfNamesThatStartsWithLetter.length;
+      min = 0;
+      position = Math.floor(Math.random() * (max - min) + min);
+      return arrayOfNamesThatStartsWithLetter[position];
+    }
+  }
+};
+
+const getPerson = (options) => {
+  let fName, lName, age;
+  if (!options) {
+    const min = 18;
+    const max = 50;
+    fName = getFirstName();
+    lName = getLastName();
+    age = Math.floor(Math.random() * (max - min) + min);
+    return {
+      fName,
+      lName,
+      age,
+    };
+  } else {
+    let { min, max } = options;
+    fName = getFirstName();
+    lName = getLastName();
+    age = Math.floor(Math.random() * (max - min) + min);
+    return {
+      fName,
+      lName,
+      age,
+    };
+  }
+};
+
+const getPersonList = (options) => {
+  const { amt } = options;
+  const persons = new Array(amt).fill(0);
+  const personArr = [];
+  if (amt) {
+    persons.map(() => {
+      personArr.push(getPerson());
+    });
+
+    return personArr;
+  }
+};
+
+const getNameList = (options) => {
+  const { amt } = options;
+  const nameCnt = new Array(amt).fill(0);
+  const firstNamesArr = [];
+  if (amt) {
+    nameCnt.map(() => {
+      firstNamesArr.push(getPerson());
+    });
+
+    return firstNamesArr;
+  }
+};
+
+const naijaFaker = () =>{
+  const getFirstName = (letterToStartWith) => {
+    let max = firstNames.length - 1;
+    let min = 0;
+    let position;
+    // random fName
+    if (!letterToStartWith) {
+      position = Math.floor(Math.random() * (max - min) + min);
+      return firstNames[position];
+    } else {
+      // random fName that starts with letterToStartWith
+      if (letterToStartWith.length <= 1) {
+        const arrayOfNamesThatStartsWithLetter = [];
+        firstNames.map((firstName) => {
+          if (firstName.startsWith(letterToStartWith)) {
+            arrayOfNamesThatStartsWithLetter.push(firstName);
+          }
+        });
+  
+        max = arrayOfNamesThatStartsWithLetter.length;
+        min = 0;
+        position = Math.floor(Math.random() * (max - min) + min);
+        return arrayOfNamesThatStartsWithLetter[position];
+      }
+    }
+  };
+  
+  const getLastName = (letterToStartWith) => {
+    let max = lastNames.length - 1;
+    let min = 0;
+    let position;
+    // random fName
+    if (!letterToStartWith) {
+      position = Math.floor(Math.random() * (max - min) + min);
+      return lastNames[position];
+    } else {
+      // random fName that starts with letterToStartWith
+      if (letterToStartWith.length <= 1) {
+        const arrayOfNamesThatStartsWithLetter = [];
+        lastNames.map((lastName) => {
+          if (lastName.startsWith(letterToStartWith)) {
+            arrayOfNamesThatStartsWithLetter.push(lastName);
+          }
+        });
+  
+        max = arrayOfNamesThatStartsWithLetter.length;
+        min = 0;
+        position = Math.floor(Math.random() * (max - min) + min);
+        return arrayOfNamesThatStartsWithLetter[position];
+      }
+    }
+  };
+  
+  const getPerson = (options) => {
+    let fName, lName, age;
+    if (!options) {
+      const min = 18;
+      const max = 50;
+      fName = getFirstName();
+      lName = getLastName();
+      age = Math.floor(Math.random() * (max - min) + min);
+      return {
+        fName,
+        lName,
+        age,
+      };
+    } else {
+      let { min, max } = options;
+      fName = getFirstName();
+      lName = getLastName();
+      age = Math.floor(Math.random() * (max - min) + min);
+      return {
+        fName,
+        lName,
+        age,
+      };
+    }
+  };
+  
+  const getPersonList = (options) => {
+    const { amt } = options;
+    const persons = new Array(amt).fill(0);
+    const personArr = [];
+    if (amt) {
+      persons.map(() => {
+        personArr.push(getPerson());
+      });
+  
+      return personArr;
+    }
+  };
+  
+  const getNameList = (options) => {
+    const { amt } = options;
+    const nameCnt = new Array(amt).fill(0);
+    const firstNamesArr = [];
+    if (amt) {
+      nameCnt.map(() => {
+        firstNamesArr.push(getPerson());
+      });
+  
+      return firstNamesArr;
+    }
+  };
+}
+module.exports = {
+  getFirstName,
+  getLastName,
+  getPerson,
+  getPersonList,
+  getNameList,
+  naijaFaker
+};
+
